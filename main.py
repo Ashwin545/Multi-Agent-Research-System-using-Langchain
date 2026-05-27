15d2e8fa48c9a7b9a4b2dd8cf31cdfc533cd5b42
Ashwin545 <119476510+Ashwin545@users.noreply.github.com>
2026-05-27 16:43:13 +0530

    agents working and tested

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.tools.tools import web_search,scrape_url
+from src.pipeline.pipeline import run_research_pipeline
 
-r = web_search.invoke("What is the latest research on AGI?")
-print(r)
+
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
